88c32c113035340f5faaeb17b151a6162e5fc259
applyfreereviewrequestformteam-ctrl <apply.free.review.request.form.team@gmail.com>
2026-01-14 16:35:28 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+live-voice-app/
+│
+├── backend/
+│   ├── server.js
+│   ├── package.json
+│   ├── socket.js
+│   ├── config/
+│   │   └── db.js
+│   ├── models/
+│   │   ├── User.js
+│   │   ├── Room.js
+│   │   └── Gift.js
+│   ├── routes/
+│   │   ├── auth.js
+│   │   ├── room.js
+│   │   └── wallet.js
+│   └── admin/
+│       └── admin.js
+│
+└── frontend/
